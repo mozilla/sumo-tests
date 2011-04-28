909c35bb5389d435cadbeaecfe3f086a460befff
vish-moz <vish.mozilla@gmail.com>
2011-04-27 17:41:19 -0700

    corrected env. variables

--- vars.py
+++ vars.py
@@ -1,30 +1,23 @@
 class ConnectionParameters:
     server = "localhost"
-    #server = "qa-selenium.mv.mozilla.com"
     port = 4444
-    #browser = "IE-8;en-us;Win7-second"
-    #browser = "IE-8;en-us;Win7"
-    browser = "*chrome"
-    #browser = "Firefox-default;en-us;MacOSX6"
+    browser = "Firefox-default;en-us;MacOSX6"
     baseurl = "http://support.allizom.org"
-    #baseurl = "http://support-release.allizom.org"
-    #baseurl = "http://support.mozilla.com"
     baseurl_ssl = "https://support.allizom.org"
-    #baseurl_ssl = "https://support-release.allizom.org"
     authurl = "http://support-release.allizom.org"
     authurlssl = "https://support-release.allizom.org"
     page_load_timeout = 120000
 
 class ConfigOptions:
     #Environment list
-    Smoketests = ["IE-8;en-us;Win7-second"]
-    
-#    BFT = [
-#    "Firefox-default;en-us;Win7-second"
-#    ]
-    BFT = [
-    "*chrome"
+    Smoketests = [
+    "Firefox-default;en-us;MacOSX6"
     ]
+    
+    BFT = [
+    "Firefox-default;en-us;Win7-second"
+    ]
+    
     FFT = [
     "Firefox-default;en-us;MacOSX6",
     "Firefox-default;en-us;Win7-second"
