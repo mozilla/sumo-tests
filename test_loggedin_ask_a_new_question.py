a99fcb02062083bd7949d2393be902caecb25f52
vish-moz <vish.mozilla@gmail.com>
2011-05-06 13:35:43 -0700

    changed the AAQ test to Page Object Model

--- test_loggedin_ask_a_new_question.py
+++ test_loggedin_ask_a_new_question.py
@@ -1,56 +1,59 @@
+#!/usr/bin/env python
+'''
+Created on May 6, 2011
+
+@author: mozilla
+'''
+
 from selenium import selenium
 import vars
 import unittest
 import datetime
-import time
-import sumo_functions
+
+import questions_page
+import login_page
 import sumo_test_data
 
-
-class loggedin_ask_a_new_question(unittest.TestCase):
-
+class TestAAQ(unittest.TestCase):
+    
     def setUp(self):
-        self.selenium = selenium(
-        vars.ConnectionParameters.server,
-        vars.ConnectionParameters.port,
-        vars.ConnectionParameters.browser,
-        vars.ConnectionParameters.baseurl)
+        self.selenium = selenium(vars.ConnectionParameters.server, vars.ConnectionParameters.port, vars.ConnectionParameters.browser, vars.ConnectionParameters.baseurl)
         self.selenium.start()
-        self.selenium.set_timeout(vars.ConnectionParameters.page_load_timeout)
-        self.accounts = sumo_test_data.SUMOtestData()
-        self.functions = sumo_functions.SUMOfunctions()
-
-    def test_loggedin_ask_a_new_question(self):
-        sel = self.selenium
-        sumo_func = sumo_functions.SUMOfunctions()
-        # Description: This testcase verifies adding a new question
-        timestamp = sel.get_eval("new Date().getTime()")
-        self.functions.login(0, sel)
-        sumo_func.open(sel, '/')
-        sel.open("/en-US/kb/ask")
-        sel.click("link=Ask a new question in the forum")
-        sel.wait_for_page_to_load(vars.ConnectionParameters.page_load_timeout)
-        # Adding a new question
-        sel.click("css=ul.select-one > li > a")
-        sel.wait_for_page_to_load(vars.ConnectionParameters.page_load_timeout)
-        sel.click("css=ul.select-one > li > a")
-        sel.wait_for_page_to_load(vars.ConnectionParameters.page_load_timeout)
-        question_to_ask = "automation test question %s" %(datetime.date.today())
-        sel.type("search", question_to_ask)
-        sel.click("css=input[value='Ask this']")
-        sel.wait_for_page_to_load(vars.ConnectionParameters.page_load_timeout)
-        sel.click("show-form-btn")
-        sel.wait_for_page_to_load(vars.ConnectionParameters.page_load_timeout)
-        sel.type("id_content", "This is a test. " + timestamp)
-        sel.type("id_troubleshooting", "troubleshooting info")
-        sel.click("css=input[value='Post Question']")
-        sel.wait_for_page_to_load(vars.ConnectionParameters.page_load_timeout)
-        time.sleep(3)
-        self.failUnless(sel.is_text_present("Your question has been posted"), "Did not get confirmation for Question: %s \r\n" %(question_to_ask))
-        self.failUnless(sel.is_text_present(question_to_ask))
+        self.timeout = vars.ConnectionParameters.page_load_timeout
+        self.selenium.set_timeout(self.timeout)
 
     def tearDown(self):
         self.selenium.stop()
 
+                
+    def test_that_posting_question_works(self):
+        """Posts a question to /questions"""
+        sel                 = self.selenium
+        login_po      = login_page.LoginPage(sel)
+        questions_po = questions_page.QuestionsPage(sel)
+        timestamp = sel.get_eval("new Date().getTime()")
+        q_to_ask = "automation test question %s" %(datetime.date.today())
+        q_details = "This is a test. " + timestamp
+
+
+        user_info       = sumo_test_data.SUMOtestData().getUserInfo(0)
+        uname           = user_info['username']
+        pwd             = user_info['password']
+        
+        ''' login '''
+        login_po.log_in(uname, pwd)
+        
+        """ go to the /questions/new page and post a question
+        """
+        questions_po.go_to_ask_new_questions_page()
+        questions_po.click_firefox_product_link()
+        questions_po.click_category_problem_link()
+        questions_po.type_question(q_to_ask)
+        questions_po.click_provide_details_button()
+        questions_po.fill_up_questions_form(q_details)
+        
+        self.failUnless(sel.is_text_present(q_to_ask))
+        self.failUnless(sel.is_text_present(q_details))
+        
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main()                                               
