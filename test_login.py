531ce3152a1d6e75997a582cb1e88a5dda9ecff6
Rebecca Billings <rerlin@gmail.com>
2011-05-12 14:06:06 -0700

    updated login test

--- test_login.py
+++ test_login.py
@@ -3,48 +3,43 @@ Created on Aug 12, 2010
 
 @author: mozilla
 '''
-
 from selenium import selenium
-import vars
 import unittest
-
 import login_page
-import support_home_page
 import sumo_test_data
+import vars
+import re
+import support_home_page
 
 class TestLogin(unittest.TestCase):
-
-
+    
     def setUp(self):
-        self.selenium = selenium(vars.ConnectionParameters.server, vars.ConnectionParameters.port, vars.ConnectionParameters.browser, vars.ConnectionParameters.baseurl)
+        self.selenium = selenium(
+        vars.ConnectionParameters.server,
+        vars.ConnectionParameters.port,
+        vars.ConnectionParameters.browser,
+        vars.ConnectionParameters.baseurl)
         self.selenium.start()
-        self.timeout = vars.ConnectionParameters.page_load_timeout
-        self.selenium.set_timeout(self.timeout)
+        self.selenium.set_timeout(vars.ConnectionParameters.page_load_timeout)
 
     def tearDown(self):
         self.selenium.stop()
-
-
+        
     def test_login(self):
-        """tests login & logout
-        """
+        """ tests login & logout """
         sel = self.selenium
         login_page_obj   = login_page.LoginPage(sel)
-        support_page_obj = support_home_page.SupportHomePage(sel)
         
-        user_info       = sumo_test_data.SUMOtestData().getUserInfo(0)
+        user_adm = 1
+        user_info       = sumo_test_data.SUMOtestData().getUserInfo(user_adm)
         uname           = user_info['username']
         pwd             = user_info['password']
         
         ''' Login '''
-        
-        login_page_obj.log_in(uname, pwd)              
-        
-        login_page_obj.go_to_login_page()
+        login_page_obj.log_in(uname, pwd)
         
         ''' Logout '''
         login_page_obj.log_out()
-        support_page_obj.verify_page_title(support_page_obj.title)
 
         
 if __name__ == "__main__":
