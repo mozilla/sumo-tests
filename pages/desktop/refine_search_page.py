0aa080a8b0977476d70d5da92b459c0175bac269
Leah Klearman <lklrmn@gmail.com>
2012-03-09 13:02:15 -0800

    WD for seach pages
    12:58:55-~/github/klrmn-sumo-tests$ mozrunwd --runxfail -k search
    ============================= test session starts
    ==============================
    platform darwin -- Python 2.6.1 -- pytest-2.2.3
    collected 35 items
    
    tests/desktop/test_search.py ...
    
    ====================== 32 tests deselected by '-ksearch'
    =======================
    =================== 3 passed, 32 deselected in 22.54 seconds
    ===================

--- pages/desktop/refine_search_page.py
+++ pages/desktop/refine_search_page.py
@@ -4,74 +4,70 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from pages.desktop.base import Base
+from selenium.webdriver.common.by import By
 
 
 class RefineSearchPage(Base):
     """
        'Advanced Search' page.
     """
-    _page_title = 'Search'
+    _page_title = 'Search | Firefox Help'
     _page_url = '/en-US/search?a=2'
-    _article_search_box = "kb_q"
-    _post_search_box = 'support_q'
-    _post_tags_box = 'id_q_tags'
-    _thread_search_box = 'discussion_q'
-    _search_button_kb = "css=input[name='w'][value='1']+div.submit-search > input[type='submit']"
-    _search_button_support = "css=input[name='w'][value='2']+div.submit-search > input[type='submit']"
-    _search_button_disc = "css=input[name='w'][value='4']+div.submit-search > input[type='submit']"
-    _kb_cat_check_box = "css=input#id_category_0"
-    _kb_tab = "css=div#search-tabs > ul > li:nth-child(1) > a"
-    _support_questions_tab = "css=div#search-tabs > ul > li:nth-child(2) > a"
-    _forums_tab = "css=div#search-tabs > ul > li:nth-child(3) > a"
-    _asked_by_box = "id_asked_by"
-    _search_results_list = "css=div.result.question"
+    _article_search_box = (By.ID, 'kb_q')
+    _post_search_box = (By.ID, 'support_q')
+    _post_tags_box = (By.ID, 'id_q_tags')
+    _thread_search_box = (By.ID, 'discussion_q')
+    _search_button_kb = (By.CSS_SELECTOR, 'input[name="w"][value="1"]+div.submit-search > input[type="submit"]')
+    _search_button_support = (By.CSS_SELECTOR, 'input[name="w"][value="2"]+div.submit-search > input[type="submit"]')
+    _search_button_disc = (By.CSS_SELECTOR, 'input[name="w"][value="4"]+div.submit-search > input[type="submit"]')
+    _kb_cat_check_box = (By.CSS_SELECTOR, 'input#id_category_0')
+    _kb_tab = (By.CSS_SELECTOR, 'div#search-tabs > ul > li:nth-child(1) > a')
+    _support_questions_tab = (By.CSS_SELECTOR, 'div#search-tabs > ul > li:nth-child(2) > a')
+    _forums_tab = (By.CSS_SELECTOR, 'div#search-tabs > ul > li:nth-child(3) > a')
+    _asked_by_box = (By.ID, 'id_asked_by')
+    _search_results_list = (By.CSS_SELECTOR, 'div.result.question')
 
     def go_to_refine_search_page(self):
-        self.selenium.open(self._page_url)
-        self.selenium.wait_for_page_to_load(self.timeout)
+        self.open(self._page_url)
         self.is_the_current_page
 
     def click_support_questions_tab(self):
-        self.selenium.click(self._support_questions_tab)
+        self.selenium.find_element(*self._support_questions_tab).click()
 
     def type_in_asked_by_box(self, text):
-        self.selenium.type(self._asked_by_box, text)
+        self.selenium.find_element(*self._asked_by_box).send_keys(text)
 
     def click_search_button_support(self):
-        self.selenium.click(self._search_button_support)
-        self.selenium.wait_for_page_to_load(self.timeout)
+        self.selenium.find_element(*self._search_button_support).click()
 
     def do_search_on_knowledge_base(self, search_query, search_page_obj):
-        self.selenium.click(self._kb_tab)
-        self.selenium.type(self.article_search_box, search_query)
-        self.selenium.click(self._search_button_kb)
-        self.selenium.wait_for_page_to_load(self.timeout)
+        self.selenium.find_element(*self._kb_tab).click()
+        self.selenium.find_element(*self.article_search_box).send_keys(search_query)
+        self.selenium.find_element(*self._search_button_kb).click()
         search_page_obj.is_the_current_page
 
     def do_search_on_support_questions(self, search_query, search_page_obj):
-        self.selenium.click(self._support_questions_tab)
-        self.selenium.type(self.post_search_box, search_query)
-        self.selenium.click(self._search_button_support)
-        self.selenium.wait_for_page_to_load(self.timeout)
+        self.selenium.find_element(*self._support_questions_tab).click()
+        self.selenium.find_element(*self.post_search_box).send_keys(search_query)
+        self.selenium.find_element(*self._search_button_support).click()
         search_page_obj.is_the_current_page
 
     def do_search_tags_on_support_questions(self, search_query, search_page_obj):
-        self.selenium.click(self._support_questions_tab)
-        self.selenium.type(self._post_tags_box, search_query)
-        self.selenium.click(self._search_button_support)
-        self.selenium.wait_for_page_to_load(self.timeout)
+        self.selenium.find_element(*self._support_questions_tab).click()
+        self.selenium.find_element(*self._post_tags_box).send_keys(search_query)
+        self.selenium.find_element(*self._search_button_support).click()
         search_page_obj.is_the_current_page
 
     def do_search_on_discussion_forums(self, search_query, search_page_obj):
-        self.selenium.click(self._forums_tab)
-        self.selenium.type(self._thread_search_box, search_query)
-        self.selenium.click(self._search_button_disc)
-        self.selenium.wait_for_page_to_load(self.timeout)
+        self.selenium.find_element(*self._forums_tab).click()
+        self.selenium.find_element(*self._thread_search_box).send_keys(search_query)
+        self.selenium.find_element(*self._search_button_disc).click()
         search_page_obj.is_the_current_page
 
-    def is_kb_cat_checked(self):
-        return self.selenium.is_checked(self._kb_cat_check_box)
+#    not used
+#    def is_kb_cat_checked(self):
+#        return self.selenium.find_element(*self._kb_cat_check_box).is_checked
 
     @property
     def search_result_count(self):
-        return self.selenium.get_css_count(self._search_results_list)
+        return len(self.selenium.find_elements(*self._search_results_list))
