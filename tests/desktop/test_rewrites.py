aad419c86e7d4f8f6683098391633f98bfc4c55c
Stephen Donner <stephen.donner@gmail.com>
2012-05-31 12:44:35 -0700

    Fixing WINNT Options Window - General Panel redirect test

--- tests/desktop/test_rewrites.py
+++ tests/desktop/test_rewrites.py
@@ -33,7 +33,7 @@ class TestRedirects:
         ('/1/firefox/4.0/Linux/en-US/firefox-help/', '/en-US/home?as=u'),
         ('/1/firefox/4.0/Linux/en-US/firefox-f1/', '/en-US/home?as=u'),
         ('/1/firefox/4.0/Linux/en-US/firefox-osxkey/', '/en-US/home?as=u'),
-        ('/1/firefox/4.0/WINNT/en-US/prefs-main/', '/en-US/kb/Options window - General panel?as=u'),
+        ('/1/firefox/4.0/WINNT/en-US/prefs-main/', '/en-US/kb/startup-home-page-download-settings?redirectlocale=en-US&as=u&redirectslug=Options+window+-+General+panel'),
         ('/1/firefox/4.0/Darwin/en-US/prefs-main/', '/en-US/kb/startup-home-page-download-settings?redirectlocale=en-US&as=u&redirectslug=Options+window+-+General+panel'),
         ('/1/firefox/4.0/Linux/en-US/prefs-main/', '/en-US/kb/startup-home-page-download-settings?redirectlocale=en-US&as=u&redirectslug=Options+window+-+General+panel'),
         ('/1/firefox/4.0/WINNT/en-US/prefs-clear-private-data/', '/en-US/kb/remove-recent-browsing-search-and-download-history?redirectlocale=en-US&as=u&redirectslug=Clear+Recent+History'),
