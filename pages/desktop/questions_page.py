291822d4e06af5f29926af37b2bb73f6257a38d3
Teodosia <teodosia.pop@gmail.com>
2012-09-06 16:29:37 +0300

    Fix TestQuestions.test_that_posting_question_works Updated the locators needed for the test to pass.

--- pages/desktop/questions_page.py
+++ pages/desktop/questions_page.py
@@ -7,6 +7,7 @@ from pages.desktop.base import Base
 from selenium.webdriver.common.by import By
 from unittestzero import Assert
 
+
 class QuestionsPage(Base):
     """
     'Ask a Question' landing page.
@@ -21,7 +22,7 @@ class QuestionsPage(Base):
     _questions_list_locator = (By.CSS_SELECTOR, 'ol.questions > li')
     _question_list_link_locator = (By.CSS_SELECTOR, 'h2 > a')
     _solved_or_unsolved_text_locator = (By.CSS_SELECTOR, 'div.thread-meta > span')
-    
+
     def click_ask_new_questions_link(self):
         self.selenium.find_element(*self._ask_question_link_locator).click()
         return AskNewQuestionsPage(self.testsetup)
@@ -63,16 +64,15 @@ class AskNewQuestionsPage(Base):
     """
     _page_title = 'Ask a Question | Firefox Help'
     _page_url = '/en-US/questions/new'
-    _firefox_product_first_link_locator = (By.CSS_SELECTOR, 'ul.select-one > li > a')
+    _firefox_product_first_link_locator = (By.CSS_SELECTOR, '#product-picker li:nth-child(1) > a')
     _category_prob_first_link_locator = (By.CSS_SELECTOR, 'ul.select-one > li > a')
     _type_question_box_locator = (By.NAME, 'search')
-    _ask_this_button_locator = (By.CSS_SELECTOR, 'input[value="Ask this"]')
-    _none_of_these_button_locator = (By.CSS_SELECTOR, 'input[value *="None"]')
-    _provide_details_button_locator = (By.ID, 'show-form-btn')
+    _ask_this_button_locator = (By.CSS_SELECTOR, '#ask-search-form .btn.btn-important')
+    _none_of_these_button_locator = (By.CSS_SELECTOR, 'form .btn.btn-submit')
     _q_content_box_locator = (By.ID, 'id_content')
     _q_site_box_locator = (By.ID, 'id_sites_affected')
     _q_trouble_box_locator = (By.ID, 'id_troubleshooting')
-    _q_post_button_locator = (By.CSS_SELECTOR, 'input[value="Post Question"]')
+    _q_post_button_locator = (By.CSS_SELECTOR, 'li.submit button.btn')
     _sort_solved_link_locator = (By.CSS_SELECTOR, 'a[href*=filter=solved]')
     _sort_unsolved_link_locator = (By.CSS_SELECTOR, 'a[href*=filter=unsolved]')
     _sort_no_replies_link_locator = (By.CSS_SELECTOR, 'a[href*=filter=no-replies]')
@@ -89,8 +89,8 @@ class AskNewQuestionsPage(Base):
         self.selenium.find_element(*self._type_question_box_locator).send_keys(question_to_ask)
         self.selenium.find_element(*self._ask_this_button_locator).click()
 
-    def click_provide_details_button(self):
-        self.selenium.find_element(*self._provide_details_button_locator).click()
+    def click_none_of_these_solve_my_problem_button(self):
+        self.selenium.find_element(*self._none_of_these_button_locator).click()
 
     def fill_up_questions_form(self, question_to_ask, q_text='details', q_site='www.example.com', q_trouble='no addons'):
         self.selenium.find_element(*self._q_content_box_locator).send_keys(q_text)
@@ -122,7 +122,6 @@ class ViewQuestionPage(Base):
                          "Expected page title: %s. Actual page title: %s" % \
                          (question_name + self._page_title, page_title))
 
-        
     @property
     def question(self):
         return self.selenium.find_element(*self._question_locator).text
