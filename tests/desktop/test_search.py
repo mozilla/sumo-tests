c8321d039fc0dce566cd1de8e9feb7db25854b5d
yoshiyokotani <yoshi.yokotani@gmail.com>
2013-05-30 10:47:59 +0200

    change of variable names and addition of comments

--- tests/desktop/test_search.py
+++ tests/desktop/test_search.py
@@ -6,7 +6,6 @@ from unittestzero import Assert
 import pytest
 from pages.desktop.page_provider import PageProvider
 from pages.desktop.search_page import SearchPage
-from selenium.webdriver.common.keys import Keys
 
 class TestSearch:
 
@@ -30,18 +29,18 @@ class TestSearch:
     def test_user_flow_to_forum_post(self, mozwebqa):
            
         #1. start on the home page
-        home_page = PageProvider(mozwebqa).home_page()
+        home_pg = PageProvider(mozwebqa).home_page()
         
         #2. type "Firefox crashed"
         #3. hit Enter 
-        search_page = SearchPage(mozwebqa)
-        search_page.do_search_on_search_query(self.forum_search_term+"ed")
+        search_pg = SearchPage(mozwebqa)
+        search_pg.do_search_on_search_query(self.forum_search_term+"ed")
         
         #4. In the results list there are two types of results: 
         #   Forum and KB. Click on a forum result. 
         #   (Url is in the forum of /questions/[some number])
         #5. A complete forum thread should be displayed.
-        Assert.true(search_page.is_result_present, "result page is not present.")
-        result_thread_title = search_page.get_result_question_text()
+        Assert.true(search_pg.is_result_present, "result page is not present.")
+        result_thread_title = search_pg.get_result_question_text()
         Assert.contains(self.forum_search_term,result_thread_title)
-        search_page.click_question_link(self.forum_search_term)                
+        search_pg.click_question_link(self.forum_search_term)                
